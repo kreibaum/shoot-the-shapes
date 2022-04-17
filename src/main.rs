11f2fd1dccf226ebf5df56a22b6ff6e7e4e789c9
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2022-04-17 17:15:05 +0200

    Get a rotating triangle

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,42 @@
 use bevy::prelude::*;
+use bevy_prototype_lyon::prelude::*;
+
+const BACKGROUND_COLOR: Color = Color::rgb(0.1, 0.1, 0.1);
 
 fn main() {
     App::new()
+        .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
+        .add_plugin(ShapePlugin)
+        .insert_resource(ClearColor(BACKGROUND_COLOR))
+        .add_startup_system(setup_system)
+        .add_system(rotate_triangle)
+        .add_system(bevy::input::system::exit_on_esc_system)
         .run();
 }
 
 #[derive(Component)]
-struct Person;
+struct Enemy;
 
-#[derive(Component)]
-struct Name(String);
+fn setup_system(mut commands: Commands) {
+    let shape = shapes::RegularPolygon {
+        sides: 3,
+        feature: shapes::RegularPolygonFeature::Radius(200.0),
+        ..shapes::RegularPolygon::default()
+    };
 
-pub struct HelloPlugin;
-struct GreetTimer(Timer);
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-            .add_startup_system(add_people)
-            .add_system(greet_people);
-    }
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands
+        .spawn_bundle(GeometryBuilder::build_as(
+            &shape,
+            DrawMode::Fill(FillMode::color(Color::CYAN)),
+            Transform::default().with_rotation(Quat::from_axis_angle(Vec3::Z, 1.0)),
+        ))
+        .insert(Enemy);
 }
 
-fn add_people(mut commands: Commands) {
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Elaina Proctor".to_string()));
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Renzo Hume".to_string()));
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Zayna Nieves".to_string()));
-}
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in query.iter() {
-            println!("hello {}!", name.0);
-        }
+fn rotate_triangle(time: Res<Time>, mut query: Query<&mut Transform, With<Enemy>>) {
+    for mut transform in query.iter_mut() {
+        transform.rotate(Quat::from_axis_angle(Vec3::Z, time.delta_seconds()));
     }
 }
