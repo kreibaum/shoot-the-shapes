44fd19c94b0252f22cf6173c62b201aa456227e8
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2022-04-17 16:08:48 +0200

    Finish book. (Is is very short)

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,9 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_startup_system(add_people)
-        .add_system(hello_world)
-        .add_system(greet_people)
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
         .run();
-}
-
-fn hello_world() {
-    println!("hello world!");
 }
 
 #[derive(Component)]
@@ -17,6 +12,17 @@ struct Person;
 
 #[derive(Component)]
 struct Name(String);
+
+pub struct HelloPlugin;
+struct GreetTimer(Timer);
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people)
+            .add_system(greet_people);
+    }
+}
 
 fn add_people(mut commands: Commands) {
     commands
@@ -33,8 +39,10 @@ fn add_people(mut commands: Commands) {
         .insert(Name("Zayna Nieves".to_string()));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("hello {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}!", name.0);
+        }
     }
 }
